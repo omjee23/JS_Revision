6f7786adaf65c49f41e207074b066e7e527f67e1
Om Jee Pal <om22@navgurukul.org>
2024-12-03 20:29:34 +0530

    forLoop

---
+++ loops/forLoop.js
@@ -0,0 +1,47 @@
+for( let i=1; i<=10; i++){
+    console.log(i);
+    
+}
+
+
+// prnit odd number 1 to 100 ;
+
+for (let i=1; i<=100; i++){
+    if(i%2 != 0){
+        console.log(i);
+        
+    } 
+}
+
+
+// print even number 1 to 100
+
+for (let i=0; i<=100 ; i++){
+    if(i%2 == 0){
+        console.log(i);
+        
+    }
+}
+
+//nested Loop 
+
+for (let i=1; i<=3; i++) {
+    console.log("nested Loop");
+    for (let j=1; j<4; j++){
+        console.log(j);
+        
+    }
+}
+
+
+// while loop 
+
+let i = 1;
+while(i < 5){
+    console.log(i);
+    i++;
+    
+}
+
+
+
