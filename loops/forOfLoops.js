1c59eb9b7fd3cbfe146c66abab6c9d7971703c16
Om Jee Pal <om22@navgurukul.org>
2024-12-04 15:39:55 +0530

    loop Practice

---
+++ loops/forOfLoops.js
@@ -0,0 +1,9 @@
+let fruits = ["mango", "banana", "lime", "orange", "apple"];
+for (fruit of fruits){
+    console.log(fruit); 
+}
+
+for(char of "omjee Pal"){
+    console.log(char);
+    
+}
---
+++ loops/nestedoforOfLoop.js
@@ -0,0 +1,6 @@
+let heros = [["Mharana", "sambha ji","Ajad", "subhas Daa", "savarkar"], ["Mharani Lakshmibai", "durgawati", "chenmma", "padmini"]];
+for(list of heros){
+    for(name of list){
+        console.log(name);  
+    }
+}
