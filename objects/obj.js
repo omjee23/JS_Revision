13cbcb0b01894143f176f714d818653c40333a73
Om Jee Pal <om22@navgurukul.org>
2024-12-05 14:10:53 +0530

    object Practice

---
+++ objects/obj.js
@@ -0,0 +1,19 @@
+const student = {
+    name: "omji",
+    age: 23,
+    marks: 99,
+    city: "Gurgaon"
+}
+
+console.log(student);
+
+
+const post = {
+    userName : "omjipal",
+    content : "this is my first post", 
+    repost : 4,
+    tags : ["@Om", "@Hva"],
+
+}
+console.log(post);
+
